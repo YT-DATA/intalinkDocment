d77bf4995fd43d9d0a67bc934e175f44d717c52c
chenyuheng <1010747237@qq.com>
2024-11-14 16:36:04 +0800

    base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:"/intalinkDocment/",
   title: "intalink",
   description: "intalink",
   themeConfig: {
